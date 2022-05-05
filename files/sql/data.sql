26a39b12e6694a6d3b2e769e2fe4786551a94342
Ivy-Walobwa <ivywalobwa@gmail.com>
2022-05-05 11:22:07 +0300

    Update data.sql

--- files/sql/data.sql
+++ files/sql/data.sql
@@ -1,3 +1,5 @@
+-- Insert into ksqldb
+
 INSERT INTO emergencies (name, reason, area) VALUES ('Liam', 'allergy', 'Florida');
 INSERT INTO emergencies (name, reason, area) VALUES ('Fiona', 'dizziness', 'Orlando');
 INSERT INTO emergencies (name, reason, area) VALUES ('Mike', 'pain', 'Florida');
@@ -8,3 +10,15 @@ INSERT INTO emergencies (name, reason, area) VALUES ('Louise', 'dizziness', 'Haw
 INSERT INTO emergencies (name, reason, area) VALUES ('Ivor', 'choking', 'New York');
 INSERT INTO emergencies (name, reason, area) VALUES ('Louise', 'pain', 'Florida');
 INSERT INTO emergencies (name, reason, area) VALUES ('Beckham', 'allergy', 'New York');
+
+-- Insert into Redpanda
+{"name":"Liam", "reason": "allergy", "area": "Florida"}
+{"name":"Fiona", "reason": "dizziness", "area": "Orlando"}
+{"name":"Mike", "reason": "pain", "area": "Florida"}
+{"name":"Louise", "reason": "allergy", "area": "Orlando"}
+{"name":"Steven", "reason": "stroke", "area": "New York"}
+{"name":"Liam", "reason": "pain", "area": "Florida"}
+{"name":"Louise", "reason": "dizziness", "area": "Hawai"}
+{"name":"Ivor", "reason": "choking", "area": "New York"}
+{"name":"Louise", "reason": "pain", "area": "Florida"}
+{"name":"Beckham", "reason": "allergy", "area": "New York"}
